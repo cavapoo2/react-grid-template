69f79e1c8a22236eeb2507b3b750cdf4f7a696ce
cavapoo2 <forpetessake@protonmail.com>
2018-10-02 22:07:56 +0100

    add more detail to grid

--- src/grid.js
+++ src/grid.js
@@ -4,11 +4,21 @@ import './Grid.css';
 const Grid = () => {
     return (
         <div className="grid boxed lines small">
-            <span className="box01 header">Header</span>
-            <span className="box02 content">Content</span>
+            <span className="box01 header">Options Menu</span>
+{/*            <span className="box06 header2">Header</span>*/}
+            <span className="box02 content">Emblem Image</span>
+            {/*
             <span className="box03 leftside">Leftside</span>
             <span className="box04 rightside">Rightside</span>
+            */}
             <span className="box05 footer">Footer</span>
+            <span className="box07 empty">Empty</span>
+            <span className="box08 iconmenu">IconsMenu</span>
+            <span className="box09 mainmenu">MainMenu</span>
+            <span className="box10 imageshowcase">ImageshowCase</span>
+            <span className="box11 featuredProductsHeader">FeaturedProductsHeader</span>
+            <span className="box12 featuredProductsImages">FeaturedProductsImages</span>
+
     </div> 
     );
 }
