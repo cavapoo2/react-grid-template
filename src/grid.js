505fcd5b27bac3227579d1f3d40c07cebc8b002c
cavapoo2 <forpetessake@protonmail.com>
2018-10-02 17:38:56 +0100

    add more rows and some text

--- src/grid.js
+++ src/grid.js
@@ -4,11 +4,11 @@ import './Grid.css';
 const Grid = () => {
     return (
         <div className="grid boxed lines small">
-            <span className="box01 header"></span>
-            <span className="box02 content"></span>
-            <span className="box03 leftside"></span>
-            <span className="box04 rightside"></span>
-            <span className="box05 footer"></span>
+            <span className="box01 header">Header</span>
+            <span className="box02 content">Content</span>
+            <span className="box03 leftside">Leftside</span>
+            <span className="box04 rightside">Rightside</span>
+            <span className="box05 footer">Footer</span>
     </div> 
     );
 }
